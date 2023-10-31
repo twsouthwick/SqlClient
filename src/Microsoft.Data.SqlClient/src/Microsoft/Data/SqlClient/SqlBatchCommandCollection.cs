f1dc37f6b6255b1598caaf1731f5486be8be8da3
David Engel <v-davidengel@microsoft.com>
2023-10-31 16:17:42 -0700

    Removing public SqlBatch APIs on netfx

--- src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlBatchCommandCollection.cs
+++ src/Microsoft.Data.SqlClient/src/Microsoft/Data/SqlClient/SqlBatchCommandCollection.cs
@@ -2,6 +2,8 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+#if NET6_0_OR_GREATER
+
 using System.Collections.Generic;
 using System.Data.Common;
 using Microsoft.Data.Common;
@@ -78,3 +80,4 @@ namespace Microsoft.Data.SqlClient
 
     }
 }
+#endif
